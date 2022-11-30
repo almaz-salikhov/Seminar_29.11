90e25a3810d2854a07ba3932ac7cd7de8a2033bf
Almaz Salikhov <almaz.salikhov@gmail.com>
2022-11-30 17:03:12 +0200

    HW Task 1 Done

--- HomeWork/HW_task1/Program.cs
+++ HomeWork/HW_task1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая на вход принимает два числа и выдает, какое большее, а какое меньшее.
+Console.Write ("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write ("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine()); 
+
+if(a > b) 
+{
+    int max = a; int min = b;
+    Console.Write("Максимальное из двух: ");
+    Console.WriteLine(max);
+    Console.Write("Минимальное из двух: ");
+    Console.WriteLine(min);
+}
+else
+{
+    int max = b; int min = a;
+    Console.Write("Максимальное из двух: ");
+    Console.WriteLine(max);
+    Console.Write("Минимальное из двух: ");
+    Console.WriteLine(min);
+}
