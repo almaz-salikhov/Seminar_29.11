98a241a6b07ca1ee8674c62208641afe9099263c
Almaz Salikhov <almaz.salikhov@gmail.com>
2022-11-30 19:34:15 +0200

    HW Task 3 Done

---
+++ HomeWork/HW_task3/Program.cs
@@ -0,0 +1,8 @@
+﻿//Является ли число четным?
+
+Console.Write ("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num%2 == 0) Console.WriteLine("Число является четным");
+
+else Console.WriteLine("Число является нечетным");
