b5b1161dd62e6a80d3068fc6ab03f4a142612a0e
Almaz Salikhov <almaz.salikhov@gmail.com>
2022-12-02 19:57:41 +0200

    Added changes in HW task3 - separate lines for yes/no

--- HomeWork/HW_task3/Program.cs
+++ HomeWork/HW_task3/Program.cs
@@ -3,6 +3,7 @@
 Console.Write ("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if(num%2 == 0) Console.WriteLine("Число является четным");
-
-else Console.WriteLine("Число является нечетным");
+if(num%2 == 0) 
+Console.WriteLine("Число является четным");
+else 
+Console.WriteLine("Число является нечетным");
